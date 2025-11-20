f07837b3824f9d3cd6b30f888c344babd045d6b8
cveimarsena <vcelis@sena.edu.co>
2025-11-19 23:53:59 -0500

    ajustes finales

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -10,7 +10,7 @@
     .row.justify-content-center
       .col-lg-12.mb-5.bgs.p-4.brad
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum. ")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="Síntesis del componente formativo sobre la valoración de apoyos. El diagrama expone cuatro bloques principales: antecedentes, concepto, proceso técnico y aspectos adicionales. Cada bloque despliega sus subtemas: descripción y antecedentes; habilidades de la persona facilitadora y qué no es una valoración; contenido de la solicitud, preparación y entrevista previa; y finalmente asistencias personales, ayudas vivas y apoyos tecnológicos.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
